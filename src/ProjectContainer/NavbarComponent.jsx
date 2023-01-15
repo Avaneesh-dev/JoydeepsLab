758b91de665117ad85bf9cd98083f1c6f4de2716
Avaneesh Srivastava <avaneesh2812@gmail.com>
2023-01-15 23:31:43 +0530

    created teamComponent

--- src/ProjectContainer/NavbarComponent.jsx
+++ src/ProjectContainer/NavbarComponent.jsx
@@ -7,39 +7,39 @@ function NavbarComponent(props) {
   const [collapsed, setCollapsed] = useState(true);
   const toggleNavbar = () => setCollapsed(!collapsed);
   return (
-    <div className='Screen'>
-      <Navbar color='inherit' dark expand="md" className="col-12">
+    <div className=''>
+      <Navbar color='inherit' light expand="md" className="col-12 navtext">
         <NavbarBrand className="col-md-3 col-sm-6">Dr. Joydeep Banerjee's Lab</NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} />
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar className='col-md-9 text-center'>
           <NavItem className="col-md-2">
-              <NavLink href="home" style={{color:"white"}}>Home</NavLink>             
+              <NavLink href="home">Home</NavLink>             
             </NavItem>
             <NavItem className="col-md-2">
-            <NavLink href="research" style={{color:"white"}}>Research</NavLink>              
+            <NavLink href="research">Research</NavLink>              
             </NavItem>            
             <NavItem className="col-md-2">
-            <NavLink href="team" style={{color:"white"}}>Members</NavLink>              
+            <NavLink href="team">Members</NavLink>              
             </NavItem>
             <NavItem className="col-md-2">
-            <NavLink href="contact" style={{color:"white"}}>Contact Us</NavLink>              
+            <NavLink href="contact">Contact Us</NavLink>              
             </NavItem>
             <NavItem className="col-md-2">
-            <NavLink href="careers" style={{color:"white"}}>Careers</NavLink>              
+            <NavLink href="careers">Careers</NavLink>              
             </NavItem>
             <NavItem className="col-md-2">
-            <NavLink href="alumni" style={{color:"white"}}>Alumni</NavLink>              
+            <NavLink href="alumni">Alumni</NavLink>              
             </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
       <div className="my-10 row">
-      <div className="h1 col-md-9 text-center">
-      {/*<video className="embed-responsive" id="video-background" muted autoPlay loop>
-          <source src={require("./pexels-marian-croitoru-6042317.mp4")} type="video/mp4" />
-          <source src={require("./pexels-marian-croitoru-6042317.mp4")} type="video/ogg" />
-  </video>*/}
+        <div className="h1 col-md-9 text-center ">
+          <video className="embed-responsive" id="video-background" muted autoPlay loop>
+          <source src={require("./stock1.mp4")} type="video/mp4" />
+          <source src={require("./stock1.mp4")} type="video/ogg" />
+        </video>
         <p className="h1">Laboratory of Plant Molecular Biology & Transgenics</p>
         <p className="h1">Department of Agricultural and Food Engineering</p>
         <p className="h2">Indian Institute of Technology Kharagpur, West Bengal, India </p>
--- src/ProjectContainer/TeamComponent.jsx
+++ src/ProjectContainer/TeamComponent.jsx
@@ -1,13 +1,61 @@
 import React from 'react'
 import "./Main.css"
+import Joydeep from './joydeep.jpeg'
 function TeamComponent() {
-  return (
-    <div className="my-10">
-      <div className="header">
-        <header className="display-5 p-5" >MEMBERS</header>
-      </div>
-      <div>
+  const phdMembers = [{name:"Debarati Nandi", research:"Agricultural Biotechnology", description: "not uploaded", src:[Joydeep]},{name:"Hena Gain", research:"Agricultural Biotechnology", description: "not uploaded", src:[Joydeep]},{name:"Sreya Ghosal", research:"Plant Molecular Biology", description: "not uploaded", src:[Joydeep]},{name:"Ankita Chakraborty", research:"Stress Physiology in Plants", description: "not uploaded", src:[Joydeep]},{name:"Shivani", research:"Stress Physiology in Plants", description: "not uploaded", src:[Joydeep]}];
+  const pgMembers=[{name:"Avaneesh Srivastava",research:"Machine Learning in Crop Science", src:[Joydeep]},{name:"Pallavi Agrawal",research:"Machine Learning in Crop Science", src:[Joydeep]},{name:"Mansi Gupta", research:"Machine Learning in Crop Science",src:[Joydeep]},{name:"Swarnali Bharracharyya",research:"Machine Learning in Crop Science", src:[Joydeep]},{name:"Akhila Yallappa Olekar",research:"Machine Learning in Crop Science", src:[Joydeep]}]
 
+  const phdMember = phdMembers.map((member)=> {
+    return(
+      <div className="text-end col-sm-6 col-md-4 row">
+        <div className="text-end col-6">          
+          <p className='text-decoration-underline h3'>{member.name}</p>
+          <p className='h6 text-muted'>Area of Research:<br />{member.research}</p>
+          <p className='lead d-none d-sm-block'>{member.description}</p>
+        </div>
+        <div className='col-6'>
+          <img className="img-fluid rounded img-thumbnail" src={member.src} />
+        </div>
+      </div>
+    );
+  })
+  const pgMember = pgMembers.map((member)=> {
+    return(
+      <div className="text-end col-sm-6 col-md-4 row">
+        <div className="text-end col-6">          
+          <p className='text-decoration-underline h3'>{member.name}</p>
+          <p className='h6 text-muted'>Area of Research:<br />{member.research}</p>
+        </div>
+        <div className='col-6'>
+          <img className="img-fluid rounded img-thumbnail" src={member.src} />
+        </div>
+      </div>
+    );
+  })
+  return (
+    <div className="my-10 teams">
+      <div className="header">
+        <header className="display-5 p-5">MEMBERS</header>
+      </div>
+      <div className='row'>
+        <p className='h2 fw-bold'>DOCTORAL FELLOWS</p>
+        {phdMember}        
+      </div>
+      <div className='row'>
+        <p className='h2 fw-bold'>M.TECH RESEARCH FELLOWS</p>
+        {pgMember}        
+      </div>
+      <div className='row'>
+        <p className='h2 fw-bold'>M.S. RESEARCH FELLOWS</p>
+        <div className="text-end col-sm-6 col-md-4 row">
+        <div className="text-end col-6">          
+          <p className='text-decoration-underline h3'>Sophia Ofosuhemaa Duodu</p>
+          <p className='h6 text-muted'>Area of Research:<br />Agricultural Biotechnology</p>
+        </div>
+        <div className='col-6'>
+          <img className="img-fluid rounded img-thumbnail" src={Joydeep}/>
+        </div>
+      </div>      
       </div>
     </div>
   )
